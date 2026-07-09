b4e78dfa67b5d40c37e46fff563ba077c362eac3
MkMan100 <95576058+MkMan100@users.noreply.github.com>
2026-07-09 21:37:15 +0100

    Update SynthVoice.h correzione switch

--- Sambuca/Source/SynthVoice.h
+++ Sambuca/Source/SynthVoice.h
@@ -193,10 +193,10 @@ public:
 
         switch (filterType)
         {
-            case 0: voiceFilter1.setMode(juce::dsp::LadderFilterMode::LP); break;
-            case 1: voiceFilter1.setMode(juce::dsp::LadderFilterMode::HP); break;
-            case 2: voiceFilter1.setMode(juce::dsp::LadderFilterMode::BP); break;
-            case 3: voiceFilter1.setMode(juce::dsp::LadderFilterMode::BP); break; 
+            case 0: voiceFilter1.setMode(juce::dsp::LadderFilterMode::lowPass); break;
+            case 1: voiceFilter1.setMode(juce::dsp::LadderFilterMode::highPass); break;
+            case 2: voiceFilter1.setMode(juce::dsp::LadderFilterMode::bandPass); break;
+            case 3: voiceFilter1.setMode(juce::dsp::LadderFilterMode::bandPass); break; 
         }
         
         float modulatedCutoff1 = juce::jlimit (20.0f, 20000.0f, baseCutoff1 + (lastLfoValue * 5000.0f));
